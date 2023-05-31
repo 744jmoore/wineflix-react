05e41949a9aa4b3c4e794ffd8036f6c48cf41a56
Jacob-Moore_lmig <Jacob.Moore@LibertyMutual.com>
2023-05-31 19:48:25 -0400

    added button Components review fa icons needed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPlay, faInfoCircle } from "@fortawesome/free-solid-svg-icons"
 import Navigation from "./Navigation";
+import SecondaryNavigation from "./SecondaryNavigation";
+import HeroContent from "./HeroContent";
 
 const App = () => {
   return (
@@ -12,52 +12,9 @@ const App = () => {
  
       <nav className="navigation">
       <Navigation />
-{/*         <section className="primary-navigation">
-          <h1>WineFlix</h1>
-          <ul className="navigation-links">
-            <li className="active">
-              <a href="https://developer.mozilla.org">Home</a>
-            </li>
-            <li>
-              <a href="https://developer.mozilla.org">Whites</a>
-            </li>
-            <li>
-              <a href="https://developer.mozilla.org">Reds</a>
-            </li>
-            <li>
-              <a href="https://developer.mozilla.org">Blends</a>
-            </li>
-          </ul>
-        </section> */}
-        <section className="secondary-navigation">
-          <span className="user-avatar">
-            <a href="https://developer.mozilla.org">S</a>
-          </span>
-        </section>
+        <SecondaryNavigation />
       </nav>
-      <div className="hero-content">
-        <h2>
-          <img alt="Rex Goliath" src="/rex-goliath-logo.png" />
-        </h2>
-        <h3>His Royal Majesty is back.</h3>
-        <p>
-          America's most unhinged rooster is out of retirement, and this time? He's mad as hell.  Experience the unapologetic flavor burst of Rex Goliath.
-        </p>
-        <ul className="controls">
-          <li>
-            <button className="primary-action">
-            <FontAwesomeIcon icon={faPlay} />
-            <span>Drink</span>
-            </button>
-          </li>
-          <li>
-            <button className="secondary-action">
-              <FontAwesomeIcon icon={faInfoCircle} />
-              <span>More Info</span>
-            </button>
-          </li>
-        </ul>
-      </div>
+      <HeroContent />
     </header>
     <main>
       <section className="category">
