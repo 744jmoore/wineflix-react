e62ae76b4738364e13b731ca9e475605bfa9412a
Jacob-Moore_lmig <Jacob.Moore@LibertyMutual.com>
2023-06-06 09:36:05 -0400

    added alt to button.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -4,7 +4,7 @@ const Buttons = ({ b }) => {
     return (
         <li>
             <button className={b.type} key={b.id}>
-                <img src={b.icon} style={{width: 20, height:20}}/>
+                <img src={b.icon} alt="graphical_image_button" style={{width: 20, height:20}}/>
                 <span>{b.title}</span>
             </button>
         </li>
